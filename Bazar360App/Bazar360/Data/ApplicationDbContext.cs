da3ddf4707ea0c4ac8c7035ce761ff8dc2b92af5
mmraihan <mm_raihan@live.com>
2022-07-26 15:31:30 +0600

    Migrate to .NET 6 with Customer Area.

---
+++ Bazar360App/Bazar360/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bazar360.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bazar360.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
---
+++ Bazar360App/Bazar360/Models/ProductTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bazar360.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Product Type")]
+        public string ProductType { get; set; }
+    }
+}
