7c599622200269e4d007761559b38e239b100a2b
mmraihan <mm_raihan@live.com>
2022-07-26 23:18:24 +0600

    Add ProductType Page.

---
+++ Bazar360App/Bazar360/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,20 @@
+﻿using Bazar360.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazar360.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+    }
+}
