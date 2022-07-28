b96a21ae1544befe5baa0a856f3d43dfe533b1d5
mmraihan <mm_raihan@live.com>
2022-07-28 17:52:11 +0600

    Create method with UI and Image File in Product controller.

--- Bazar360App/Bazar360/Areas/Admin/Controllers/ProductController.cs
+++ Bazar360App/Bazar360/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Bazar360.Data;
+using Bazar360.Models;
 using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace Bazar360.Areas.Admin.Controllers
 {
@@ -7,14 +9,45 @@ namespace Bazar360.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public ProductController(ApplicationDbContext db)
+        private readonly IHostingEnvironment _he;
+
+        public ProductController(ApplicationDbContext db, IHostingEnvironment he)
         {
-            _db=db;
+            _db = db;
+            _he = he;
         }
         public IActionResult Index()
         {
             var products = _db.Products.ToList();
             return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, IFormFile image)
+        {
+            if (ModelState.IsValid) //Server Side Valid
+            {
+                if (image !=null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+
+                    await  image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image ="Images/" + image.FileName;
+                }  
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+
+                TempData["save"] = "Product saved successfully"; //Alertify
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
 
 
