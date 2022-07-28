93518e7c49b2d8cd59220b35d82f61085276e101
mmraihan <mm_raihan@live.com>
2022-07-28 15:52:13 +0600

    Add Product Controller with Index page and method.

---
+++ Bazar360App/Bazar360/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Bazar360.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazar360.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            return View(products);
+        }
+
+
+    }
+}
