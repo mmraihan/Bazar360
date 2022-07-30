19588a17826c68120a8b32af76e18d8682fca6ea
mmraihan <mm_raihan@live.com>
2022-07-31 03:23:24 +0600

    Edit Post Method.

--- Bazar360App/Bazar360/Areas/Admin/Controllers/ProductController.cs
+++ Bazar360App/Bazar360/Areas/Admin/Controllers/ProductController.cs
@@ -85,5 +85,37 @@ namespace Bazar360.Areas.Admin.Controllers
         }
 
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid) //Server Side Valid
+            {
+                if (product.ImageFile != null)
+                {
+                    string folder = "Images/";
+                    folder += Guid.NewGuid().ToString() + "_" + product.ImageFile.FileName;
+                    string serverFolder = Path.Combine(_webHost.WebRootPath, folder);
+                    product.ImageUrl = "/" + folder; //save url to db
+
+                    await product.ImageFile.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+
+                }
+
+                if (product.ImageFile == null)
+                {
+                    product.ImageUrl = "/Images/No-image-found.jpg";
+                }
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+
+                TempData["save"] = "Product updated successfully"; //Alertify
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+
     }
 }
