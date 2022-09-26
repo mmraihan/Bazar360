59a86c1dec10f809a22f330037162908bdce2729
mmraihan <mm_raihan@live.com>
2022-09-26 18:55:44 +0600

    Add Create User Controllere with action method and view.

---
+++ Bazar360App/Bazar360/Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Bazar360.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazar360.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class UserController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            var result =await _userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View();
+        }
+
+    }
+}
